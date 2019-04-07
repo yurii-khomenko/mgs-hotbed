5a4b7bc358fb44b924177e482054f45d7d9f099b
yykhomenko <yykhomenko@gmail.com>
2019-04-07 16:38:39 +0300

    crear

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,8 @@ ESP8266System sys({
 void setup(void) {
   sys.setup();
   sys.setupDHT(D3, DHT22);
-
-  // pinMode(D2, OUTPUT);
-
 }
 
 void loop(void) {
   sys.loop();
-
-  // sys.on(D2);
-  // delay(100);
-  // sys.off(D2);
 }
