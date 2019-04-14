4347ebcb76e4b45eecbdc732fce6fa1f6f02c605
yykhomenko <yykhomenko@gmail.com>
2019-04-14 19:03:56 +0300

    init clion

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <ESP8266System.h>
 
 ESP8266System sys({
-  "greenhouse", "mgs", "hotbed",
-  "INSIDIUS 2.4GHz", "scalable"
-});
+                          "greenhouse", "mgs", "hotbed",
+                          "INSIDIUS 2.4GHz", "scalable"
+                  });
 
 #define DHT_SENSOR_PIN        D3
 #define HUMIDIFIER_PIN        D4
@@ -11,13 +11,13 @@ ESP8266System sys({
 #define VENTILATION_PIN       D6
 
 void setup(void) {
-  sys.setup();
-  sys.setupDHT(DHT_SENSOR_PIN, DHT22);
-  sys.setupHumidifier(HUMIDIFIER_PIN, HUMIDIFIER_STATE_PIN);
-  sys.setupVentilation(VENTILATION_PIN);
-  sys.setupGigrostat(96, 99);
+    sys.setup();
+    sys.setupDHT(DHT_SENSOR_PIN, DHT22);
+    sys.setupHumidifier(HUMIDIFIER_PIN, HUMIDIFIER_STATE_PIN);
+    sys.setupVentilation(VENTILATION_PIN);
+    sys.setupGigrostat(96, 99);
 }
 
 void loop(void) {
-  sys.loop();
+    sys.loop();
 }
