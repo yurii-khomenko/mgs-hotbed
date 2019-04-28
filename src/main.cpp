534249d1f17afb0eb3aae6ee6f5fddaf96e0604f
yykhomenko <yykhomenko@gmail.com>
2019-04-28 20:17:13 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -26,3 +26,46 @@ void setup(void) {
 void loop(void) {
   sys.loop();
 }
+
+//// constants won't change. Used here to set a pin number :
+//const int ledPin =  13;      // the number of the LED pin
+//
+//// Variables will change :
+//int ledState = LOW;             // ledState used to set the LED
+//
+//// Generally, you should use "unsigned long" for variables that hold time
+//// The value will quickly become too large for an int to store
+//unsigned long previousMillis = 0;        // will store last time LED was updated
+//
+//// constants won't change :
+//const unsigned long interval = 12UL*60UL*60UL*1000UL;   // Change the LED every 12 hours
+//
+//void setup() {
+//  // set the digital pin as output:
+//  pinMode(ledPin, OUTPUT);
+//}
+//
+//void loop() {
+//  // here is where you'd put code that needs to be running all the time.
+//
+//  // check to see if it's time to toggle the LED; that is, if the
+//  // difference between the current time and last time you blinked
+//  // the LED is bigger than the interval at which you want to
+//  // blink the LED.
+//  unsigned long currentMillis = millis();
+//
+//  if (currentMillis - previousMillis >= interval) {
+//    // save the time you should have toggled the LED
+//    previousMillis += interval;
+//
+//    // if the LED is off turn it on and vice-versa:
+//    if (ledState == LOW) {
+//      ledState = HIGH;
+//    } else {
+//      ledState = LOW;
+//    }
+//
+//    // set the LED with the ledState of the variable:
+//    digitalWrite(ledPin, ledState);
+//  }
+//}
